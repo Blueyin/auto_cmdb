19279bea0dd3b8fb944717cbb3d437f9a3d301c1
Blueyin <296120962@qq.com>
2018-03-24 20:30:58 +0800

    update

--- app/backend/exec_jobs.py
+++ app/backend/exec_jobs.py
@@ -1,5 +1,5 @@
-# /usr/bin/python2.7
-# encoding:utf-8
+#/usr/bin/python2.7
+#encoding:utf-8
 
 import urllib
 import jenkins
@@ -8,23 +8,22 @@ from datetime import datetime
 import sys
 import MySQLdb
 
-conn = MySQLdb.connect(host="127.0.0.1", user="test", passwd="test_1234", db="cmdb", charset="utf8")
+conn = MySQLdb.connect(host="127.0.0.1",user="test",passwd="test_1234",db="cmdb",charset="utf8")
 
-
-def import_release_sql(ret, jid):
-    conn = MySQLdb.connect(host="127.0.0.1", user="test", passwd="test_1234", db="cmdb", charset="utf8")
+def import_release_sql(ret,jid):
+    conn = MySQLdb.connect(host="127.0.0.1",user="test",passwd="test_1234",db="cmdb",charset="utf8")
     cursor = conn.cursor()
-    # if jid !='':
-    # print ret
-    # sql_del = '''DELETE FROM `app_running_list` WHERE jid = %s and name = %s'''
+    #if jid !='':
+    #print ret
+    #sql_del = '''DELETE FROM `app_running_list` WHERE jid = %s and name = %s'''
     set_names = '''set names utf8'''
     sql = '''INSERT INTO `app_running_list`
                 (`name`, `user`, `fun`, `start_time`, `end_time`, `sum_time`, `status`,`jid`,`action`,`result`,`command`)
                 VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
-    # print sql,ret
+    #print sql,ret
     try:
 	cursor.execute(set_names)
-	# cursor.execute(sql_del,jid)
+	#cursor.execute(sql_del,jid)
 	cursor.execute(sql, ret)
 	cursor.execute("COMMIT")
     except:
@@ -33,12 +32,11 @@ def import_release_sql(ret, jid):
 	conn.close()
     return True
 
-
-def update_release_sql(name, jid, status):
-    conn = MySQLdb.connect(host="127.0.0.1", user="test", passwd="test_1234", db="cmdb", charset="utf8")
+def update_release_sql(name,jid,status):
+    conn = MySQLdb.connect(host="127.0.0.1",user="test",passwd="test_1234",db="cmdb",charset="utf8")
     cursor = conn.cursor()
     set_names = '''set names utf8'''
-    sql = "UPDATE app_running_list SET  jid='%s',status='%s' WHERE name='%s'" % (jid, status, name)
+    sql = "UPDATE app_running_list SET  jid='%s',status='%s' WHERE name='%s'" % (jid,status,name)
     cursor.execute(set_names)
     try:
 	cursor.execute(sql)
@@ -49,13 +47,11 @@ def update_release_sql(name, jid, status):
 	conn.close()
     return True
 
-
-def update_operate_sql(jid, ret):
-    conn = MySQLdb.connect(host="127.0.0.1", user="test", passwd="test_1234", db="cmdb", charset="utf8")
+def update_operate_sql(jid,ret):
+    conn = MySQLdb.connect(host="127.0.0.1",user="test",passwd="test_1234",db="cmdb",charset="utf8")
     cursor = conn.cursor()
     set_names = '''set names utf8'''
-    sql = "UPDATE app_schedule_running_list SET  status='%s',end_time='%s',sum_time='%s',result='%s',command='%s' WHERE jid='%s'" % (
-	ret[0], ret[1], ret[2], ret[3], ret[4], jid)
+    sql = "UPDATE app_schedule_running_list SET  status='%s',end_time='%s',sum_time='%s',result='%s',command='%s' WHERE jid='%s'" % (ret[0],ret[1],ret[2],ret[3],ret[4],jid)
     cursor.execute(set_names)
     try:
 	cursor.execute(sql)
@@ -65,32 +61,29 @@ def update_operate_sql(jid, ret):
     else:
 	conn.close()
     return True
-
 
 def insert_hostlist_sql(params):
-    conn = MySQLdb.connect(host="127.0.0.1", user="test", passwd="test_1234", db="cmdb", charset="utf8")
+    conn = MySQLdb.connect(host="127.0.0.1",user="test",passwd="test_1234",db="cmdb",charset="utf8")
     cursor = conn.cursor()
     set_names = '''set names utf8'''
     sql = '''INSERT INTO `app_hostlist`
                 (`hostname`, `ip`, `bianhao`, `idc_name`,`application`)
                 VALUES ( %s, %s, %s, %s, %s)'''
     cursor.execute(set_names)
-    # try:
-    cursor.executemany(sql, params)
+    #try:
+    cursor.executemany(sql,params)
     cursor.execute("COMMIT")
-    # except:
-    # conn.rollback()
-    # else:
+    #except:
+    #conn.rollback()
+    #else:
     conn.close()
     return True
 
-
-def insert_operate_sql(jid, ret):
-    conn = MySQLdb.connect(host="127.0.0.1", user="test", passwd="test_1234", db="cmdb", charset="utf8")
+def insert_operate_sql(jid,ret):
+    conn = MySQLdb.connect(host="127.0.0.1",user="test",passwd="test_1234",db="cmdb",charset="utf8")
     cursor = conn.cursor()
     set_names = '''set names utf8'''
-    sql = "UPDATE app_running_list SET  status='%s',end_time='%s',sum_time='%s',result='%s',command='%s' WHERE jid='%s'" % (
-	ret[0], ret[1], ret[2], ret[3], ret[4], jid)
+    sql = "UPDATE app_running_list SET  status='%s',end_time='%s',sum_time='%s',result='%s',command='%s' WHERE jid='%s'" % (ret[0],ret[1],ret[2],ret[3],ret[4],jid)
     cursor.execute(set_names)
     try:
 	cursor.execute(sql)
@@ -101,9 +94,8 @@ def insert_operate_sql(jid, ret):
 	conn.close()
     return True
 
-
 def select_salt_return(jid):
-    conn = MySQLdb.connect(host="127.0.0.1", user="test", passwd="test_1234", db="cmdb", charset="utf8")
+    conn = MySQLdb.connect(host="127.0.0.1",user="test",passwd="test_1234",db="cmdb",charset="utf8")
     cursor = conn.cursor()
     set_names = '''set names utf8'''
     sql = "select * from app_salt_return where jid='%s'" % (jid)
@@ -120,19 +112,18 @@ def select_salt_return(jid):
 	    time.sleep(1)
 	if n >= 10:
 	    return False
-    if sel[0][5] == '1' or sel[0][5] == 'success' or sel[0][5] == 'True':
+    if sel[0][5] == '1' or sel[0][5] == 'success' or  sel[0][5] == 'True':
 	return True
     else:
 	return False
 
-
-def select_schedule_sql(jid, time_now, action, command_names):
+def select_schedule_sql(jid,time_now,action,command_names):
     import datetime
-    conn = MySQLdb.connect(host="127.0.0.1", user="test", passwd="test_1234", db="cmdb", charset="utf8")
+    conn = MySQLdb.connect(host="127.0.0.1",user="test",passwd="test_1234",db="cmdb",charset="utf8")
     cursor = conn.cursor()
     set_names = '''set names utf8'''
     sql = "select * from app_salt_return where jid='%s'" % (jid)
-    # sql = "select * from app_salt_return where jid='20171228143649111939'"
+    #sql = "select * from app_salt_return where jid='20171228143649111939'"
     cursor.execute(set_names)
     results = []
     result = ""
@@ -168,9 +159,9 @@ def select_schedule_sql(jid, time_now, action, command_names):
 	    numb += 1
 	if numb == 1:
 	    result = str(ret.split('return": "')[1].split('"')[0])
-	result = result.replace("'", "\\\'")
-	result = result.replace('"', '\\\"')
-	results = ["执行成功", end_time, sum_time, result, command]
+	result = result.replace("'","\\\'")
+	result = result.replace('"','\\\"')
+	results = ["执行成功",end_time,sum_time,result,command]
     else:
 	end_time = ret.split('_stamp": "')[1].split('"')[0]
 	end_time = utc2local(end_time)
@@ -189,27 +180,27 @@ def select_schedule_sql(jid, time_now, action, command_names):
 	    numb += 1
 	if numb == 1:
 	    result = str(ret.split('return": "')[1].split('"')[0])
-	result = result.replace("'", "\\\'")
-	result = result.replace('"', '\\\"')
-	results = ["执行失败", end_time, sum_time, result, command]
+	result = result.replace("'","\\\'")
+	result = result.replace('"','\\\"')
+	results = ["执行失败",end_time,sum_time,result,command]
     if action == 'operate':
-	insert_operate_sql(jid, results)
+	insert_operate_sql(jid,results)
     elif action == 'schedule':
-	update_operate_sql(jid, results)
-    return results, status
+	update_operate_sql(jid,results)
+    return results,status
 
 
-def import_schedule_sql(ret, jid):
-    conn = MySQLdb.connect(host="127.0.0.1", user="test", passwd="test_1234", db="cmdb", charset="utf8")
+def import_schedule_sql(ret,jid):
+    conn = MySQLdb.connect(host="127.0.0.1",user="test",passwd="test_1234",db="cmdb",charset="utf8")
     cursor = conn.cursor()
     set_names = '''set names utf8'''
     sql = '''INSERT INTO `app_schedule_running_list`
                 (`name`, `user`, `fun`, `start_time`, `end_time`, `sum_time`, `status`,`jid`,`action`,`result`,`command`,`scheduleName`,`list_id`)
                 VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
-    # print sql,ret
+    #print sql,ret
     try:
 	cursor.execute(set_names)
-	# cursor.execute(sql_del,jid)
+	#cursor.execute(sql_del,jid)
 	cursor.execute(sql, ret)
 	cursor.execute("COMMIT")
     except:
@@ -231,14 +222,14 @@ def utc2local(utc_st):
     return local_st
 
 
-def insert_schedule_sql(scheduleName, results, time_now):
+
+def insert_schedule_sql(scheduleName,results,time_now):
     import datetime
-    conn = MySQLdb.connect(host="127.0.0.1", user="test", passwd="test_1234", db="cmdb", charset="utf8")
+    conn = MySQLdb.connect(host="127.0.0.1",user="test",passwd="test_1234",db="cmdb",charset="utf8")
     cursor = conn.cursor()
     sum_time = results[1] - datetime.datetime.strptime(time_now, '%Y-%m-%d %H:%M:%S')
     set_names = '''set names utf8'''
-    sql = "UPDATE app_running_list SET  end_time='%s',sum_time='%s',status='%s' WHERE name='%s'" % (
-	results[1], sum_time, results[0], scheduleName)
+    sql = "UPDATE app_running_list SET  end_time='%s',sum_time='%s',status='%s' WHERE name='%s'" % (results[1],sum_time,results[0],scheduleName)
     cursor.execute(set_names)
     try:
 	cursor.execute(sql)
@@ -250,11 +241,11 @@ def insert_schedule_sql(scheduleName, results, time_now):
     return True
 
 
-def update_schedule_sql(name, jid, status):
-    conn = MySQLdb.connect(host="127.0.0.1", user="test", passwd="test_1234", db="cmdb", charset="utf8")
+def update_schedule_sql(name,jid,status):
+    conn = MySQLdb.connect(host="127.0.0.1",user="test",passwd="test_1234",db="cmdb",charset="utf8")
     cursor = conn.cursor()
     set_names = '''set names utf8'''
-    sql = "UPDATE app_schedule_running_list SET  jid='%s',status='%s' WHERE name='%s'" % (jid, status, name)
+    sql = "UPDATE app_schedule_running_list SET  jid='%s',status='%s' WHERE name='%s'" % (jid,status,name)
     cursor.execute(set_names)
     try:
 	cursor.execute(sql)
@@ -264,8 +255,7 @@ def update_schedule_sql(name, jid, status):
     else:
 	conn.close()
     return True
-
-
 if __name__ == '__main__':
-    jobName = '运维测试2'
-    full_name = '运维测试2'
+    jobName='运维测试2'
+    full_name='运维测试2'
+
