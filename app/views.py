dce5f6ed23759ece6fe5aa55e8975ec3b922f6b0
Blueyin <296120962@qq.com>
2018-06-22 19:40:57 +0800

    添加延迟2秒

--- app/views.py
+++ app/views.py
@@ -1441,6 +1441,6 @@ def select_params(schedule_id):
 
 
 def soccer(request):
-    all_result = Soccers.objects.all().order_by("-status")[0:1]
+    all_result = Soccers.objects.all().order_by("-status")
     # all_idc = Idc.objects.all()
     return render_to_response("soccer.html", locals())
